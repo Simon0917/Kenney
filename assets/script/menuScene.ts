5fa8c0e8f40b613113cdd7652839fc17551a6e48
余南宏 <760946024@qq.com>
2020-03-24 13:04:05 +0800

    游戏菜单

---
+++ assets/script/menuScene.ts
@@ -0,0 +1,33 @@
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class menuScene extends cc.Component {
+
+    // @property(cc.Label)
+    // label: cc.Label = null;
+
+    // @property
+    // text: string = 'hello';
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+
+    }
+
+    onBtnStart(){
+        console.log('change to main scene!')
+    }
+
+    onBtnSet(){
+        console.log('setting is on!')
+    }
+
+    onBtnQuit(){
+        cc.game.end()
+    }
+    // update (dt) {}
+}
